536beadd5ae79ea29be486716a7700a04b717b37
Sabrina Lavergne <75912157+X-ynea@users.noreply.github.com>
2024-02-22 11:16:44 +0100

    Delete frontend-angular directory from Back-end branch

--- frontend-angular/src/app/home-visitor/home-visitor.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-home-visitor',
-  templateUrl: './home-visitor.component.html',
-  styleUrls: ['./home-visitor.component.css']
-})
-export class HomeVisitorComponent {
-}
