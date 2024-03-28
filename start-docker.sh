8994e739abb2588ec6732bf750c8a747379ef249
Sabrina Lavergne <75912157+X-ynea@users.noreply.github.com>
2024-03-28 14:22:42 +0100

    Update start-docker.sh

--- start-docker.sh
+++ start-docker.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
-echo "Starting application..."
+echo "Starting Docker environment..."
+
+# Chemin vers le fichier docker-compose.yaml
+DOCKER_COMPOSE_PATH="docker-compose.yaml"
+
+# Lancement de l'environnement Docker
+docker-compose -f "$DOCKER_COMPOSE_PATH" up -d
+
+echo "Docker environment started."
+
+
+echo "Starting backend..."
 
 cd Backend
 mvn spring-boot:run &
-cd ../Frontend
+cd ..
+
+echo "Starting Frontend..."
+
+cd Frontend
 npm run run-start
+cd ..
 
 sleep 5
+
 open -a Google http://localhost:4200/
 
-echo "Application stopped."
+echo "Application started."
