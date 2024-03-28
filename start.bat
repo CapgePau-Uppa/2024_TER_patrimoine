aff053bf43325d7063adc9919309ad67da975708
Sabrina Lavergne <75912157+X-ynea@users.noreply.github.com>
2024-03-28 15:23:40 +0100

    modify start.sh && start.bat

--- start.bat
+++ start.bat
@@ -1,12 +1,38 @@
 @echo off
-echo Starting application...
+
+echo Starting Docker Desktop...
+start "" "C:\Program Files\Docker\Docker\Docker Desktop.exe"
+timeout /t 10
+
+:: Chemin vers le fichier docker-compose.yaml
+set DOCKER_COMPOSE_PATH=docker-compose.yaml
+
+:: Vérification si Docker est installé
+docker -v >nul 2>&1
+if %ERRORLEVEL% neq 0 (
+    echo Docker n'est pas installé. Veuillez l'installer et réessayer.
+    exit /b 1
+)
+
+:: Lancement de l'environnement Docker
+docker-compose -f "%DOCKER_COMPOSE_PATH%" up -d
+
+echo Docker environment started.
+
+:: Lancement du backend et du frontend
+echo Starting backend and frontend...
 
 cd Backend
-mvn spring-boot:run
-cd ../Frontend
-npm run-start
+start mvn spring-boot:run
+cd ..
 
-timeout /t 5 >nul
-start Google http://localhost:4200
+cd Frontend
+start npm run run-start
+cd ..
 
-echo Application running.
+timeout /t 5
+
+:: Lancement du site web
+start http://localhost:4200/
+
+echo Webapp started.
--- start.sh
+++ start.sh
@@ -1,12 +1,35 @@
 #!/bin/bash
-echo "Starting application..."
 
-cd Backend
+echo "Starting Docker Desktop..."
+open -a Docker
+sleep 10
+
+# Chemin vers le fichier docker-compose.yaml
+DOCKER_COMPOSE_PATH="docker-compose.yaml"
+
+# Vérification si Docker est installé
+if ! command -v docker &> /dev/null
+then
+    echo "Docker n'est pas installé. Veuillez l'installer et réessayer."
+    exit 1
+fi
+
+# Lancement de l'environnement Docker
+docker-compose -f "$DOCKER_COMPOSE_PATH" up -d
+
+echo "Docker environment started."
+
+# Lancement du backend et du frontend
+echo "Starting backend and frontend..."
+
+cd ./Backend
 mvn spring-boot:run &
 cd ../Frontend
 npm run run-start
 
 sleep 5
-open -a Google http://localhost:4200/
 
-echo "Application stopped."
+# Lancement du site web
+open http://localhost:4200/
+
+echo "Webapp started."
