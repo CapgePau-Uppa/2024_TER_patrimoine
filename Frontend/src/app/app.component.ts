68b6a7792c95792edc305b5adabe499892399685
Sabrina Lavergne <75912157+X-ynea@users.noreply.github.com>
2024-04-03 16:41:05 +0200

    add button to install pwa

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { GlobalService } from "./services/global.service";
 
 @Component({
@@ -7,5 +7,32 @@ import { GlobalService } from "./services/global.service";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  deferredPrompt: any;
+  showInstallButton = false;
+
   constructor(public globalService: GlobalService) { }
+
+  @HostListener('window:beforeinstallprompt', ['$event'])
+  onBeforeInstallPrompt(e: Event): void {
+    e.preventDefault(); // Prevent the button from appearing on mobile
+    this.deferredPrompt = e;
+    this.showInstallButton = true;
+  }
+
+  installPWA(): void {
+    // Show the install prompt
+    this.deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    this.deferredPrompt.userChoice.then((choiceResult: any) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      this.deferredPrompt = null;
+      this.showInstallButton = false;
+    });
+  }
+
 }
