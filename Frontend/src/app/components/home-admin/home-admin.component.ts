319db567a4d28f2084991e50cc931968076decb8
FMM <fatouma-mahdi.mahamoud@ism.edu.sn>
2024-04-11 12:27:16 +0200

    modification Formulaire Admin

--- Frontend/src/app/components/home-admin/home-admin.component.ts
+++ Frontend/src/app/components/home-admin/home-admin.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Building } from "../../building.model";
 import { SuggestionDTO } from '../add-bat/suggestion-dto.model';
 import { HomeAdminService } from './home-admin-service.model';
+import { AddBatService } from '../add-bat/add-bat-service.model';
 
 @Component({
   selector: 'app-home-admin',
@@ -13,13 +14,29 @@ export class HomeAdminComponent implements OnInit{
   buildings: SuggestionDTO[] = [];
   buildingInfo: SuggestionDTO | null = null;
   suggestion: any = {};
+  types!: string[];
+  statuts!: string[];
+  regions!: string[];
+  departements!: string[];
+  communes!: string[];
   
-  constructor(private homeAdminService: HomeAdminService) { }
+  constructor(private homeAdminService: HomeAdminService, private addBatService : AddBatService) { }
 
   ngOnInit(): void {
     this.loadAllBuildings();
+
+    //Récupération des données pour les listes déroulantes
+    this.addBatService.getAllTypes().subscribe(types => this.types = types.sort((a, b) => a.localeCompare(b, 'fr', { sensitivity: 'base' })).slice(5));
+    this.addBatService.getAllStatuts().subscribe(statuts => this.statuts = statuts.sort((a, b) => a.localeCompare(b, 'fr', { sensitivity: 'base' })));
+    this.addBatService.getAllRegions().subscribe(regions => this.regions = regions.sort((a, b) => a.localeCompare(b, 'fr', { sensitivity: 'base' })));
+    this.addBatService.getAllDepartements().subscribe(departements => this.departements = departements.sort((a, b) => a.localeCompare(b, 'fr', { sensitivity: 'base' })));
+    this.addBatService.getAllCommunes().subscribe(communes => this.communes = communes.sort((a, b) => a.localeCompare(b, 'fr', { sensitivity: 'base' })));
+    
   }
 
+  majuscule(word: string): string {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  }
   loadAllBuildings(): void {
     this.homeAdminService.getAllSuggestions().subscribe((data: SuggestionDTO[]) => {
       this.buildings = data;
@@ -65,40 +82,33 @@ export class HomeAdminComponent implements OnInit{
         );
       }}
     
-    modifier() {
-      const inputs = document.querySelectorAll('.form input');
-      inputs.forEach(input => {
-        input.removeAttribute('disabled');
-      });
+      modificationActive: boolean = false;
+
+      modifier() {
+        // enlever la classe 'disabled' des champs
+        this.modificationActive = !this.modificationActive;
+      }
 
       
+    
+
+    // Etape
+    etapeCourante: number = 0;
+
+    afficherEtape(etapeIndex: number) {
+      this.etapeCourante = etapeIndex;
+      
+    }
+
+    etapeSuivante() {
+      if (this.etapeCourante < 2) {
+        this.etapeCourante++;
+      }
+    }
+
+    etapePrecedente() {
+      if (this.etapeCourante > 0) {
+        this.etapeCourante--;
+      }
     }
   }
-
-  /*buildings: Building[] = [];
-  buildingInfo: Building | null = null;
-
-  ngOnInit() {
-    this.buildings = this.suggestDataService.getAllBuildings();
-  }
-
-  getBuildingInformation(buildingName: string) {
-    const info = document.getElementById("container");
-    // @ts-ignore
-    info.style.display = "flex";
-
-    this.buildingInfo = this.suggestDataService.getBuilding(buildingName);
-  }
-
-  valider() {
-
-  }
-
-  modifier() {
-
-  }
-
-  annuler() {
-
-  }
-}*/
