8fd53c8c8e243930f66a6a025d4816631d114f68
Sabrina Lavergne <75912157+X-ynea@users.noreply.github.com>
2024-05-07 14:33:28 +0200

    Connexion avec docker

--- start-docker.sh
+++ start-docker.sh
@@ -3,8 +3,9 @@
 # Arrêter le script en cas d'erreur
 set -e
 
-echo "Lancement de Docker Desktop..."
 
+
+echo " ===== Lancement de Docker Desktop... ===== "
 # Vérification si Docker est installé
 if ! command -v docker &> /dev/null
 then
@@ -12,25 +13,37 @@ then
     exit 1
 fi
 
-# Si oui, on l'ouvre
+# Attendre que Docker soit prêt
 open -a Docker
 
-# Attendre que Docker soit prêt
 while ! docker system info > /dev/null 2>&1; do
   echo "En attente de l'ouverture de Docker..."
   sleep 1
 done
-
 echo "Docker est prêt."
 
-# Chemin vers le fichier docker-compose.yaml
-DOCKER_COMPOSE_PATH="docker-compose.yaml"
 
+
+echo "===== Lancement de l'environnement Docker... ====="
 # Lancement de l'environnement Docker
-docker-compose -f "$DOCKER_COMPOSE_PATH" up -d
-
+docker-compose -f "docker-compose.yaml" up -d
 echo "L'environnement Docker est lancé."
 
+
+
+echo " ===== Initialisation de la base de données... ===== "
+# Attendre que le conteneur soit prêt pour la connexion
+until docker exec 2024_ter_patrimoine-ter_bd-1 mysql -uroot -p'root' -e ";" ; do
+  echo "En attente de l'initialisation de MySQL..."
+  sleep 2
+done
+
+# Vérification de la connexion à la base de données
+echo "Vérification de la base de données..."
+docker exec 2024_ter_patrimoine-ter_bd-1 mysql -uroot -p'root' -e "USE ter_bd; SHOW TABLES"
+
+
+
+echo "===== Lancement du script de démarrage... ====="
 # Appeler le script start.sh
-echo "Lancement du script de démarrage..."
 ./start.sh
