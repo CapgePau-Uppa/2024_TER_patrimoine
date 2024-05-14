ef1dc72b2929c90e14ec5cd761d70d3ecef476ef
Sabrina Lavergne <75912157+X-ynea@users.noreply.github.com>
2024-05-14 16:35:38 +0200

    Revert "commentaire"
    This reverts commit 469507881bb3d3834d2bba797b2f0da47342db70.

--- Backend/src/main/java/TER/Backend/BackendApplication.java
+++ Backend/src/main/java/TER/Backend/BackendApplication.java
@@ -1,7 +1,14 @@
 package TER.Backend;
 
+
+import java.util.List;
+import java.util.Scanner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import TER.Backend.security.entity.User;
+import TER.Backend.security.service.UserService;
 
 
 
@@ -10,9 +17,61 @@ public class BackendApplication{ //implements CommandLineRunner
 
     public static void main(String[] args) {
         System.out.println("BACKEND DEMARRÉ");
-        SpringApplication.run(BackendApplication.class, args);
+
+        //SpringApplication.run(BackendApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(BackendApplication.class, args);
+        UserService userService = context.getBean(UserService.class);
+        boolean ok=true;
+        Scanner scanner = new Scanner(System.in);
+
+        while (ok){
+            System.out.println("1. Créer un propriétaire");
+            System.out.println("2. Lister les users");
+            System.out.println("3. Quitter");
+            System.out.print("Votre choix : ");
+            int choix = scanner.nextInt();
+            scanner.nextLine();
+            switch (choix) {
+                case 1:
+                    createProprietaire(scanner, userService);
+                    break;
+                case 2:
+                    listUsers(userService);
+                    break;
+                case 3:
+                    ok = false;
+                    break;
+                default:
+                    System.out.println("Choix invalide");
+            }
+        }
+        scanner.close();
+    }
+
+    private static List<User> listUsers(UserService userService) {
+        List<User> users = userService.findAllUsers();
+        for (User user : users) {
+            System.out.println(user);
+        }        
+        return users; // Ajout du retour de valeur
+    }
+    
+
+    private static void createProprietaire(Scanner scanner, UserService userService) {
+        System.out.print("Nom: ");
+        String nom = scanner.nextLine();
+        System.out.print("Prenom : ");
+        String prenom = scanner.nextLine();
+        System.out.print("Email : ");
+        String email = scanner.nextLine();
+        System.out.print("Mot de passe : ");
+        String mdp = scanner.nextLine();
+        userService.saveOwner(nom, prenom, email, mdp);
+        System.out.println("Propriétaire créé avec succès !");
+        
         
     }
+    
 
 }
 
--- Backend/src/main/java/TER/Backend/security/entity/User.java
+++ Backend/src/main/java/TER/Backend/security/entity/User.java
@@ -1,7 +1,10 @@
 package TER.Backend.security.entity;
 
+import TER.Backend.entities.EtatSuggestion;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
